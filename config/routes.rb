af81de6fca7f7e77b8105a0a8ef9f6e09c320c25
Lucas <lucasmteixeira97@gmail.com>
2018-09-17 10:34:32 -0400

    att

--- config/routes.rb
+++ config/routes.rb
@@ -12,16 +12,5 @@ Rails.application.routes.draw do
   resources :statuses
   devise_for :users #users/sign_in users/sign_up users/sign_out
 
-  resources :apresentacoes do
-    collection do
-      get 'colaboradores'
-      get 'patrocinadores'
-      get 'sobre'
-    end
-  end
-
-
-  root :to => "apresentacoes#index"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
